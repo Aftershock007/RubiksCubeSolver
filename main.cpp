f063a6387c029febd386dd07c80da0b0c3badca5
Arijit <biswasarijit171@gmail.com>
2025-03-06 13:37:21 +0530

    rubik's cube solver

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include "Model/RubiksCubeBitboard.cpp"
 #include "Solver/IDASTARSolver.h"
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main()
 {
     RubiksCubeBitboard cube;
